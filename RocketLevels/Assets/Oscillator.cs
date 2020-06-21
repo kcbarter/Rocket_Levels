02d6824bacf460b238d885f318f7641ef04a496b
kcbar <Kbarter@mail.greenriver.edu>
2020-06-20 22:08:06 -0700

    Objects now move to create more of a challenge in the levels

---
+++ RocketLevels/Assets/Oscillator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+
+    float movementFactor; //0 for not moved, 1 for fully moved
+
+    private Vector3 startingPostion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPostion = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period; //grows continuallly from 0
+        const float tau = Mathf.PI * 2f; //about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau); //goes from -1 to 1
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPostion + offset;
+    }
+}
